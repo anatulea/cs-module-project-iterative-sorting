0beeafd26d4d2737d66ca7785a95b75808c4e02e
Ana Tulea <anatulea14@gmail.com>
2020-09-30 15:55:32 -0700

    linear_search done

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,9 +1,11 @@
 def linear_search(arr, target):
     # Your code here
-
-
+    for i in arr:
+        if i == target:
+            return arr.index(i)
     return -1   # not found
-
+array = [2,3,4,1,8,9,0]
+print(linear_search(array,9))
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
