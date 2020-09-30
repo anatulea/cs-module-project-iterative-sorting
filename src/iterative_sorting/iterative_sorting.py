1cdee20544dcc7ab428aabf7dcbeccae50cf8b86
Ana Tulea <anatulea14@gmail.com>
2020-09-30 15:24:07 -0700

    bubble_sort works

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,25 +1,38 @@
 # WILL-DO: Complete the selection_sort() function below
-
 def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
-        cur_index = arr[i]
-        smallest_index = i
+        cur_index = i
+        smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
-        for j in range(i+1, len(array)):
-            # If a smaller element than the cur_index element is found, we re-assign the cur_index element and the minimu index
-            if array[j] < cur_index:  
-                cur_index = array[j]
+        for j in range(cur_index + 1, len(arr)):
+            # If a smaller element than the cur_index element is found, we re-assign the cur_index element and the minimum index
+            if arr[j] < arr[smallest_index]:
                 smallest_index = j
-         # If cur_index index has changed, i.e, a smaller  element has been found, then we swap that element with the ith element
-        if smallest_index  != i: 
-            array[smallest_index ], array[i] = array[i], array[smallest_index ]
         # TO-DO: swap
         # Your code here
-
+        # If cur_index index has changed, i.e, a smaller  element has been found, then we swap that element with the ith element
+        arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
     return arr
+
+# --------Method 2 -----------(selecting by the maximum value)
+# def selection_sort(arr):
+#     for i in range(len(arr)-1,0,-1):
+#         positionOfMax=0
+        
+#         # For every set of 0 to i+1
+#         for location in range(1,i+1):
+#             # Set maximum's location
+#             if arr[location]>arr[positionOfMax]:
+#                 positionOfMax = location
+
+#         temp = arr[i]
+#         arr[i] = arr[positionOfMax]
+#         arr[positionOfMax] = temp
+
+#     return arr
 
 array = [3,2,5,1,0,4]
 print(selection_sort(array))
@@ -30,10 +43,15 @@ print(selection_sort(array))
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
-
-
+    for i in range(len(arr)):
+        for j in range(0, len(arr)-i-1):
+            if arr[j]> arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
-
+    
+print("bubble_sort")
+array = [3,2,5,1,0,4]
+print(bubble_sort(array))
 '''
 STRETCH: implement the Counting Sort function below
 
