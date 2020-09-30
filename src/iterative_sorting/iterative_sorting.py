564abc8a09f07cf96fad06f509309f35f4e09945
Ana Tulea <anatulea14@gmail.com>
2020-09-30 15:45:41 -0700

    counting_sort added

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -48,7 +48,7 @@ def bubble_sort(arr):
             if arr[j]> arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
-    
+
 print("bubble_sort")
 array = [3,2,5,1,0,4]
 print(bubble_sort(array))
@@ -69,8 +69,39 @@ buckets.
 
 What is the time and space complexity of the counting sort algorithm?
 '''
-def counting_sort(arr, maximum=None):
-    # Your code here
+def counting_sort(arr):
+    if len(arr) == 0:
+        return []
 
-
-    return arr
+    max_element = int(max(arr)) 
+    min_element = int(min(arr)) 
+    range_of_elements = max_element - min_element + 1
+    # Create a count array to store count of individual 
+    # elements and initialize count array as 0 
+    count_arr = [0 for _ in range(range_of_elements)] 
+    output_arr = [0 for _ in range(len(arr))] 
+  
+    # Store count of each character 
+    for i in range(0, len(arr)): 
+       count_arr[arr[i]-min_element] += 1
+  
+    # Change count_arr[i] so that count_arr[i] now contains actual 
+    # position of this element in output array 
+    for i in range(1, len(count_arr)): 
+        count_arr[i] += count_arr[i-1] 
+  
+    # Build the output character array 
+    for i in range(len(arr)-1, -1, -1): 
+        output_arr[count_arr[arr[i] - min_element] - 1] = arr[i] 
+        count_arr[arr[i] - min_element] -= 1
+  
+    # Copy the output array to arr, so that arr now 
+    # contains sorted characters 
+    for i in range(0, len(arr)): 
+        arr[i] = output_arr[i] 
+  
+    return arr 
+print('count')
+# array =[]
+array = [-3,2,5,1,0,4]
+print(counting_sort(array))
